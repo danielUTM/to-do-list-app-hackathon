ff8873d144dd7aeab690254b80d422a7c07b4929
alex-nazemi <alexandernazemi@gmail.com>
2024-07-17 10:38:39 +0100

    deleted hello world test

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -6,6 +6,5 @@ describe('testing app file', () => {
   it("should return 200", async () => {
     const res = await request(app).get("/")
     expect(res.statusCode).toEqual(200)
-    expect(res.text).toEqual("Hello World")
   });
 });
