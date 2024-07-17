e14f064bee8e095c7545a9cdc5f6fec579237ba3
danielUTM <danielcampbell.327@icloud.com>
2024-07-17 14:43:48 +0100

    added complete task button

--- src/sqlMethods.ts
+++ src/sqlMethods.ts
@@ -8,8 +8,10 @@ function read_in_query(file: String): String {
 
 const GET_TASKS_NON_COMPLETE_QUERY = read_in_query("get_non_completed_tasks.sql");
 const INSERT_TASK_QUERY = read_in_query("insert_task.sql");
+const COMPLETE_TASK_QUERY = read_in_query("complete_task.sql");
 
 export interface Tasks {
+    id: number;
     title: String;
     summary: String;
     created_at: Date;
@@ -31,6 +33,16 @@ export async function insertTask(title:String, summary:String, due_by:Date|null,
     const client = await getClient();
 
     let sql: String = format(INSERT_TASK_QUERY, title, summary, due_by, assigned_to);
+
+    const res = await client.query(sql);
+
+    await client.end;
+}
+
+export async function updateTaskAsCompleted(id: number) {
+    const client = await getClient();
+
+    let sql: String = format(COMPLETE_TASK_QUERY, id);
 
     const res = await client.query(sql);
 
